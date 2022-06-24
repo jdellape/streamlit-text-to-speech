32b2f177e88f7dea0b9aab5a7528d60e06d5c621
John Dellape <dellapejw@gmail.com>
2022-06-24 16:21:27 -0400

    better text prompts

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from gtts import gTTS
 
-url = st.text_input('paste a url')
+url = st.text_input('Enter Link:', placeholder="Paste a link to an article and click enter to get an audio recording")
 
 def get_article_text(url):
     session = requests.Session()
@@ -15,14 +15,15 @@ def get_article_text(url):
     text_only = " ".join(text)
     return (text_only, text_with_tags)
 
-
 if url:
-    text_without_tags, text_with_tags = get_article_text(url)
-    aud_file = gTTS(text=text_without_tags, lang='en', slow=False)
-    aud_file.save("article.mp3")
-    audio_file_read = open('article.mp3', 'rb')
-    audio_bytes = audio_file_read.read()
+    with st.spinner('Gathering article text and converting to audio....'):
+        text_without_tags, text_with_tags = get_article_text(url)
+        aud_file = gTTS(text=text_without_tags, lang='en', slow=False)
+        aud_file.save("article.mp3")
+        audio_file_read = open('article.mp3', 'rb')
+        audio_bytes = audio_file_read.read()
     st.audio(audio_bytes, format='audio/mp3')
+    st.subheader('Raw Article Text')
     st.write(text_with_tags)
 
 
